842acda30374236e330843f7c07be5a4e66a5c28
Mlzna-komora <slunecnice.kd@seznam.cz>
2024-11-22 11:51:15 +0100

    nic

--- Lections homeworks/ukol_lekce_5.py
+++ Lections homeworks/ukol_lekce_5.py
@@ -2,4 +2,11 @@
 slova = []
 slovo = input("Zadej slovo")
 
-while len(slovo)  
+if len(slovo) == 5:
+    slova.append(slovo)
+    if slovo == slova:
+        print(f"Slovo {slovo} už je uložené.")
+    else:
+        print(f"Slovo není dlouhé 4 znaky")
+#print(f"Už mám uložené tři slova.")
+
