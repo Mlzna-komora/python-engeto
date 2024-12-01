bfa603c1acbc228c90532dc78f19b6a568ad8cc9
Mlzna-komora <slunecnice.kd@seznam.cz>
2024-12-01 16:03:09 +0100

    nic neco

---
+++ Homeworks/Calculator/Calculator.py
@@ -0,0 +1,29 @@
+def calculator() -> str:
+    """
+    Main function
+    """
+    print("Lets calculate!")
+
+calculator()
+
+# vypsání nabídky
+
+def nabidka():
+    operace = ["+", "-", "*", "/", "sum", "pow","gcd", "scm", "quit"]
+    spojeni = " | ".join(operace)
+    cara = "-" * len(spojeni)
+    print(cara, spojeni, cara, sep="\n")
+
+print(nabidka())
+    
+
+# Průměr
+
+
+
+
+# Umocňování
+
+
+
+
