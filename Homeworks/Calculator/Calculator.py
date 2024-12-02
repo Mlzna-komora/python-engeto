7aafdcabc03594bc898c324a25a19e1a61fd6599
Mlzna-komora <slunecnice.kd@seznam.cz>
2024-12-02 15:22:11 +0100

    safe

--- Homeworks/Calculator/Calculator.py
+++ Homeworks/Calculator/Calculator.py
@@ -1,3 +1,7 @@
+from collections import Counter
+
+
+
 def calculator() -> str:
     """
     Main function
@@ -7,23 +11,75 @@ def calculator() -> str:
 calculator()
 
 # vypsání nabídky
-
 def nabidka():
-    operace = ["+", "-", "*", "/", "sum", "pow","gcd", "scm", "quit"]
+    operace = ["+", "-", "*", "/", "avg", "sum", "pow","gcd", "scm", "quit"]
     spojeni = " | ".join(operace)
     cara = "-" * len(spojeni)
     print(cara, spojeni, cara, sep="\n")
 
-print(nabidka())
-    
+
+# KALKULAČKA
+def kalkulacka():
+    while True:
+        nabidka()
+        vyber_operace = input("vyber operaci: ")
+
+        if vyber_operace == "quit":
+            break
+        elif vyber_operace == "avg":
+            vypocti_prumer()
+        elif vyber_operace == "pow":
+            vypocti_umocneni()
+        elif vyber_operace == "sum":
+            vypocti_soucet()
+
+kalkulacka()
+            
+
+
+
 
 # Průměr
-
-
+def vypocti_prumer():
+    list_cisel = []
+    while True:
+        cislo = input("Zadej číslo: ")
+        if cislo.isinstance() and not cislo.isdigit():
+            print("Nezadal jsi číslo nebo správný tvar.")
+            continue
+        else:
+            list_cisel.append(int(cislo))
+            vysledek = sum(list_cisel)/len(list_cisel)
+            print(vysledek)
+            break
+    return vysledek
+#vypocti_prumer()
 
 
 # Umocňování
+def vypocti_umocneni() -> int:
+    cislo = int(input("Zadej číslo: "))
+    mocnina = int(input("Zadej mocninu: "))
+    vysledek = cislo**mocnina
+    print(vysledek)
+vypocti_umocneni()
 
+
+# Součet
+def vypocti_soucet():
+    list_cisel = []
+
+    while (cislo := input("Zadej číslo: ")) != "=":
+        if not cislo.isdigit():
+            break
+
+    list_cisel.append(int(cislo))
+    vysledek = sum(list_cisel)
+    print(vysledek)
+vypocti_soucet()
+
+
+# Nejvyšší společný dělitel
 
 
 
