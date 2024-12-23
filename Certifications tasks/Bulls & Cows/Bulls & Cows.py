2413a3b803b22045f3c25c956df085280571659c
Mlzna-komora <slunecnice.kd@seznam.cz>
2024-12-23 21:37:29 +0100

    ...

--- Certifications tasks/Bulls & Cows/Bulls & Cows.py
+++ Certifications tasks/Bulls & Cows/Bulls & Cows.py
@@ -24,22 +24,36 @@ print(cara)
 
 #hadani_cisla = input("Zadej číslo: ")
 
-print(cara)
-
 
 #TODO: Vytvoření random 4-ciferného čísla (číslice unikátní a nesmí začínat 0)
 # ošetření, že číslo je dlouhé 4 znaky, neobsahuje číselné znaky, atd
 
-def hadani_cisla():
-    generovani_cisla = random.sample(range(10), 4) # tuple
-    #unikatni_cislo = int("".join(map(str, generovani_cisla))) # int
-    for cislo in enumerate(generovani_cisla):
-        if cislo < tuple(range(0,4)):
-            print("není 4 ciferné")
+def tvorba_cisla():
+    while True:
+        generovani_cisla = random.sample(range(10), 4) # tuple
+        if generovani_cisla[0] == 0:
+            print("číslo začíná 0, generuji nové.")
+        else:
+            break
 
+    unikatni_cislo = int("".join(map(str, generovani_cisla))) # int
+    #print(f"Číslo v pořádku: {unikatni_cislo}")
     return unikatni_cislo
 
-print(hadani_cisla())
+#print(tvorba_cisla())
+tvorba_cisla()
+
+
+
+#def hadani_cisla(zadane_cislo: int) -> int:
+#    zadane_cislo = int(input("Zadej 4-místné číslo: "))
+#    if zadane_cislo != 4:
+#        print("Nezadal jsi 4-ciferné číslo.")
+#    if not zadane_cislo.isdigit():
+#        print("Nezadal jsi číslo.")
+#
+#    return zadane_cislo
+
 
 
 
